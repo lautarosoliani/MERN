f959fda88b7266997c26dd651e4ded46bb084b57
Lautaro <lautarosoliani@hotmail.com>
2021-07-29 00:06:16 -0300

    199 f

---
+++ src/components/layout/Barra.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Barra = () => {
+  return (
+    <header className="app-header">
+      <p className="nombre-usuario">
+        <span>Hola Lautaro</span>
+      </p>
+    </header>
+  );
+};
+
+export default Barra;
